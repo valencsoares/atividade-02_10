1cadaec38d4cb14ceaed36fddba3c6357d069e56
valencsoares <pvcampossoares558@gmail.com>
2024-10-02 15:53:42 -0400

    criado a const com as informações dos livros

---
+++ script.js
@@ -0,0 +1,6 @@
+const livros = [
+    { titulo: "A Culpa é das Estrelas", imagem: "imagens/livro1.jpg", lido: false },
+    { titulo: "Harry Potter e a Pedra Filosofal", imagem: "imagens/livro2.jpg", lido: false },
+    { titulo: "Harry Potter e as Reliquias da Morte", imagem: "imagens/livro3.jpg", lido: false },
+];
+
